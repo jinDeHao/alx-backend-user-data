0503f063cb86edef4e52644bc2cd475c285a8511
jinDeHao <o.idhmaid@gmail.com>
2024-02-10 23:28:55 +0100

    done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+import re
+"""
+filtered logger
+"""
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Regex-ing
+    """
+    new_message = message
+    for field in fields:
+        new_message = re.sub(f"(?<={field}=)[^{separator}]*",
+                              redaction,
+                              new_message,
+                              count=0,
+                              flags=0)
+    return new_message
