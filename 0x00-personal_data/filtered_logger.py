5504c0077b99a83a6490a484c858462130de40ad
jinDeHao <o.idhmaid@gmail.com>
2024-02-11 00:23:46 +0100

    Variables and return value have been annotated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,7 +31,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """
         class constructor
         """
