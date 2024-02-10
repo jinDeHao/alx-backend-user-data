b18c1f63c83194f74c472ca7c2c2bec18737e89a
jinDeHao <o.idhmaid@gmail.com>
2024-02-10 23:34:25 +0100

    requarements

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-import re
-from typing import List
 """
 filtered logger
 """
+import re
+from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """
     Regex-ing
     """
     new_message = message
     for field in fields:
-        new_message = re.sub(f"(?<={field}=)[^{separator}]*", redaction, new_message, count=0, flags=0)
+        new_message = re.sub(f"(?<={field}=)[^{separator}]*",
+                             redaction,
+                             new_message,
+                             count=0, flags=0)
     return new_message
