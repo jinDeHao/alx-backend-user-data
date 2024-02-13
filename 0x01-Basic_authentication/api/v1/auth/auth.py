a35748ebf0f408006b661dd662fe75d4a63ec785
jinDeHao <o.idhmaid@gmail.com>
2024-02-13 23:13:30 +0100

    task 3 done with authentication template

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+    def require_auth(self,
+                     path: str,
+                     excluded_paths: List[str]) -> bool:
+        """
+        public method
+        """
+        return False
+
+    def authorization_header(self,
+                             request=None) -> str:
+        """
+        public method
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        public method
+        """
+        return None
