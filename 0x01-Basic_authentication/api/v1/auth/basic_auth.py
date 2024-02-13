6bcf8ef0706392ca07a6c97ec9a6b8e22de79a2c
jinDeHao <o.idhmaid@gmail.com>
2024-02-14 00:32:09 +0100

    done 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth class
 """
 from auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,19 @@ class BasicAuth(Auth):
                 not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split("Basic ")[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """
+        Basic - Base64 decode
+        """
+        if base64_authorization_header is None or \
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            result = base64.b64decode(base64_authorization_header)
+            return result.decode('utf-8')
+        except Exception:
+            return None
