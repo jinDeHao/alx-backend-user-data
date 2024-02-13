35231bd78b09945f431fd1259844b36645b426d4
jinDeHao <o.idhmaid@gmail.com>
2024-02-13 02:30:35 +0100

    bytes bro

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ Encrypting passwords
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """
     I use the bcrypt package to perform
     the hashing (with hashpw).
