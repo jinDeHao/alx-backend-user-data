1d2934c8888e9554dcd6341b0941daa405b822bb
jinDeHao <o.idhmaid@gmail.com>
2024-02-13 02:29:51 +0100

    salt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """
+    I use the bcrypt package to perform
+    the hashing (with hashpw).
+    """
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
