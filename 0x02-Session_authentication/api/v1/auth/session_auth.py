600e60f42bbea0153129e49419175f317a7e9763
jinDeHao <o.idhmaid@gmail.com>
2024-02-15 16:59:40 +0100

    3 done with getting userid related with the session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,11 @@ class SessionAuth(Auth):
         Session_id = str(uuid.uuid4())
         self.user_id_by_session_id[Session_id] = user_id
         return Session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        User ID for Session ID
+        """
+        return None if session_id is None or\
+            not isinstance(session_id, str)\
+            else self.user_id_by_session_id[session_id]
