4e34925d30b20f243a12dc9c19bcd2a3a9b8ac84
jinDeHao <o.idhmaid@gmail.com>
2024-02-15 17:19:13 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,10 +33,14 @@ def run_before_each_request():
     """
     if auth is None:
         return
-    end_p = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    end_p = ['/api/v1/status/',
+             '/api/v1/unauthorized/',
+             '/api/v1/forbidden/',
+             '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, end_p):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
