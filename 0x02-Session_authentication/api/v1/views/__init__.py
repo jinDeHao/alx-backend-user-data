f5f7a47f032fe1c6c38480e63537384f8190b617
jinDeHao <o.idhmaid@gmail.com>
2024-02-15 23:30:07 +0100

    init

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -2,6 +2,7 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
+from models.user_session import UserSession
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
@@ -11,3 +12,4 @@ if True:
     from api.v1.views.session_auth import *
 
 User.load_from_file()
+UserSession.load_from_file()
