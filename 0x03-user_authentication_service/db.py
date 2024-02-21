ca9bb00cfdc377753a7203201938bcb33c001a33
jinDeHao <o.idhmaid@gmail.com>
2024-02-21 17:29:09 +0100

    no args

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,6 +49,8 @@ class DB:
         """
         Find user by his attributes
         """
+        if not args:
+            raise InvalidRequestError
         try:
             user = self._session.query(User).filter_by(**args).first()
         except Exception:
