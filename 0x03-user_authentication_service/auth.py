1766a2d0ba1beecc33a08e36dd358eef6b85c610
jinDeHao <o.idhmaid@gmail.com>
2024-02-21 18:23:53 +0100

    done 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ authentication methods
 from bcrypt import hashpw, gensalt, checkpw
 from db import DB, NoResultFound
 from typing import TypeVar
+import uuid
 
 
 class Auth:
@@ -15,6 +16,20 @@ class Auth:
         """
         auth contractor method"""
         self._db = DB()
+
+    def _generate_uuid(self) -> str:
+        """
+        generate a new uuid
+        """
+        return str(uuid.uuid4())
+
+    def create_session(self, email) -> str:
+        """
+        session creation
+        """
+        user = self._db.find_user_by(email=email)
+        user.session_id = self._generate_uuid()
+        return user.session_id
 
     def register_user(self, email: str, password: str) -> TypeVar("User"):
         """
