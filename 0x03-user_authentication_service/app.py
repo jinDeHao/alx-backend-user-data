83d94c1d9b0264aa2a033219ae035a4441009986
jinDeHao <o.idhmaid@gmail.com>
2024-02-21 17:54:59 +0100

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def wellcome():
+    """salut"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
