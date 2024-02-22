d7d518be8434f290cb2d7cd5220905531367238e
jinDeHao <o.idhmaid@gmail.com>
2024-02-22 20:22:22 +0100

    PUT

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,7 +74,7 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": tk}), 200
 
 
-@app.route("/reset_password", methods=['POST'])
+@app.route("/reset_password", methods=['PUT'])
 def update_password():
     """PUT /reset_password"""
     email = request.form.get("email")
