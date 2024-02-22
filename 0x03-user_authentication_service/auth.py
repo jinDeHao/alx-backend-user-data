a58248b4fed6d13db8c1ef778ddf97f9c7aa49d8
jinDeHao <o.idhmaid@gmail.com>
2024-02-22 20:11:02 +0100

    18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,6 +69,20 @@ class Auth:
         user.reset_token = _generate_uuid()
         return user.reset_token
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        self._db.update_user(
+            user.id,
+            hashed_password=_hash_password(password),
+            reset_token=None
+            )
+
     def destroy_session(self, user_id: int) -> None:
         """no more session"""
         self._db.update_user(user_id, session_id=None)
