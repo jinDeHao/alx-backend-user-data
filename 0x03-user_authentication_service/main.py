519aa830385ed048c0e1a0f709ad60c5d897b458
jinDeHao <o.idhmaid@gmail.com>
2024-02-22 23:32:55 +0100

    adv env

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -8,37 +8,50 @@ EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
 
+
 def register_user(email: str, password: str) -> None:
     """new regiter"""
     url = "http://127.0.0.1:5000/users"
     response = requests.post(url,
-    data={"email": email, "password": password})
+                             data={"email": email, "password": password})
     assert response.status_code == 200 and\
         response.json() == {"email": email, "message": "user created"}
 
 
 def log_in_wrong_password(email: str, password: str) -> None:
-    """log_in_wrong_password"""
+    """log in wrong password"""
     pass
+
+
 def log_in(email: str, password: str) -> str:
     """log in"""
     pass
+
+
 def profile_unlogged() -> None:
     """
     profile unlogged
     """
     pass
+
+
 def profile_logged(session_id: str) -> None:
     """
     profile logged
     """
     pass
+
+
 def log_out(session_id: str) -> None:
     """log out"""
     pass
+
+
 def reset_password_token(email: str) -> str:
     """reset passwd token"""
     pass
+
+
 def update_password(email: str, reset_token: str, new_password: str) -> None:
     """update passwd"""
     pass
