e810f12344d7e739ae444a730e1666c1e75f9124
jinDeHao <o.idhmaid@gmail.com>
2024-02-22 21:17:08 +0100

    main adv

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,1 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import requests
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+def register_user(email: str, password: str) -> None:
+    """new regiter"""
+    url = "http://127.0.0.1:5000/users"
+    response = requests.post(url,
+    data={"email": email, "password": password})
+    assert response.status_code == 200 and\
+        response.json() == {"email": email, "message": "user created"}
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    pass
+def log_in(email: str, password: str) -> str:
+    pass
+def profile_unlogged() -> None:
+    pass
+def profile_logged(session_id: str) -> None:
+    pass
+def log_out(session_id: str) -> None:
+    pass
+def reset_password_token(email: str) -> str:
+    pass
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    pass
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
